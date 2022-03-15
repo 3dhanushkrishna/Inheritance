805ca33ac7430c324b4821380a91dec72a5ad8fa
3dhanushkrishna <3dhanushkccd@gmail.com>
2022-03-15 13:56:57 +0530

    Inheritance example

---
+++ src/com/harman/project3/Inheritance.java
@@ -0,0 +1,14 @@
+package com.harman.project3;
+
+public class Inheritance {
+    public int Add2numbers(int a,int b)
+    {
+        Integer c = a+b;
+        return c;
+    }
+    public int Sub2numbers(int a,int b)
+    {
+        Integer c=a-b;
+        return c;
+    }
+}
